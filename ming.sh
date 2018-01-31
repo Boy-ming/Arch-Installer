4159d55df9719dcdb8fadf0e9b23b089de71cdaf
ming <huming007@126.com>
2018-01-31 13:37:30 +0800

    ming.sh

---
+++ ming.sh
@@ -0,0 +1,4 @@
+sudo pacman -S wps-office ttf-wps-fonts google-chrome zsh shadowsocks-qt5
+sudo pacman -S git screenfetch
+sudo pacman -S wqy-zenhei wqy-microhei ttf-dejavu adobe-source-code-pro-fonts
+sudo pacman -S xf86-input-synaptics
